77582f23641bb1151e8d3e086f65f25f35adbdfc
c-chee <christinechee2000@gmail.com>
2026-02-27 11:51:47 -0500

    404 (#6)
    * Feat:
    
    Add custom 404 page and add error code component
    
    * Refactor:
    
    Change 404 image and text size

--- src/components/ui/ErrorCode.jsx
+++ src/components/ui/ErrorCode.jsx
@@ -8,9 +8,9 @@ export default function ErrorCode({
     return (
         <div className={`flex flex-col place-items-center ${className}`}>
 
-            <img src='/turtle_outlined.png' alt='LRNR Turtle' className='w-[14em] md:w-[18em] lg:w-[22em] pb-1'/>
+            <img src='/turtle_outlined.png' alt='LRNR Turtle' className='w-[8em] md:w-[9em] lg:w-[10em] pb-1'/>
 
-            <h3 className='font-semibold text-[2em] md:text[3.5em]  lg:text-[4em] pb-8'>{children}</h3>
+            <h3 className='font-semibold text-[2em] md:text[2.5em]  lg:text-[3em] pb-8'>{children}</h3>
 
             <Button href='/'>Back to home</Button>
 
