7c9d48e182741ac194cc705134bb3df8f463a5a6
Christine Chee <christinechee2000@gmail.com>
2026-03-02 15:23:13 -0500

    Feat:
    Connect login and signup routes to form and add middleware to protect user dashboard

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -3,27 +3,32 @@ import bcrypt from "bcryptjs";
 import db from "@/lib/db";
 
 export async function POST(req) {
-  const { username, email, password } = await req.json();
-
   try {
+    const { username, email, password } = await req.json();
+
+    if (!username || !email || !password) {
+      return NextResponse.json({ message: "Missing fields" }, { status: 400 });
+    }
+
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const [result] = await db
       .promise()
-      .query("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", [
-        username,
-        email,
-        hashedPassword,
-      ]);
+      .query(
+        "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+        [username, email, hashedPassword]
+      );
 
     return NextResponse.json(
       { message: "User created successfully", userId: result.insertId },
-      { status: 201 },
+      { status: 201 }
     );
   } catch (error) {
-    return NextResponse.json(
-      { message: "Error creating user", error: error.message },
-      { status: 500 },
-    );
-  }
+    console.error("REGISTER ERROR:", error);
+
+      return NextResponse.json(
+        { message: error.message },
+        { status: 500 }
+      );
+    }
 }
