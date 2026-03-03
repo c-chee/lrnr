1e195fe73ca88b1719dc1a76fb825a8e464706e3
Christine Chee <christinechee2000@gmail.com>
2026-03-02 21:45:44 -0500

    Refator:
    Change navbar and footer max widths

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -19,43 +19,40 @@ export default function Footer({ type }) {
     }
 
     return (
-        <footer className='relative overflow-hidden w-full px-6 md:px-15 lg:px-18 py-12 lg:pb-10 transition-colors duration-300 bg-[var(--footer-bg)] text-[var(--background)]'>
+        <footer className='w-full bg-[var(--footer-bg)] text-[var(--background)]'>
+            <div className='max-w-7xl mx-auto px-6 md:px-10 lg:px-12 py-5'>
 
-        {/* === Grid container === */}
-        <div className='mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10'>
+                <div className='grid grid-cols-1 lg:grid-cols-2'>
 
-            {/* [ Left column ] */}
-            <div className='flex flex-col space-y-6'>
-            {/* --- Logo --- */}
-            <div>
-                <img src='/lrnr_title_white.png' alt='LRNR' className='w-[7em] block dark:hidden' />
-                <img src='/lrnr_title_black.png' alt='LRNR' className='w-[7em] hidden dark:block'/>
+                {/* Left */}
+                <div className='flex flex-col space-y-6'>
+                    <div>
+                        <img src='/lrnr_title_white.png' alt='LRNR' className='w-[7em] block dark:hidden' />
+                        <img src='/lrnr_title_black.png' alt='LRNR' className='w-[7em] hidden dark:block' />
+                    </div>
+
+                    <div className='flex flex-col space-y-2 text-md'>
+                        {links.map(link => (
+                            <Link key={link.href} href={link.href}>{link.label}</Link>
+                        ))}
+                    </div>
+                </div>
+
+                <div className='hidden lg:block'></div>
+                </div>
+
+                <div className='border-t mt-10 pt-6 border-[var(--background)]'>
+                    <p className='text-sm'>
+                        © 2026. LRNR. All rights reserved
+                    </p>
+                </div>
+
             </div>
 
-            {/* --- Links --- */}
-            <div className='flex flex-col space-y-2 text-md'>
-                {links.map(link => (
-                <Link key={link.href} href={link.href}>{link.label}</Link>
-                ))}
-            </div>
-            </div>
-
-            {/* [ Right column ] */}
-            <div className='hidden lg:block'></div>
-        </div>
-
-        {/* === Divider === */}
-        <div className='border-t mt-10 pt-6 border-[var(--background)] mx-auto'>
-            <p className='text-sm'>
-            © 2026. LRNR. All rights reserved
-            </p>
-        </div>
-
-        {/* === Turtle Mascot (Large screens only) === */}
-        <div className='hidden lg:block absolute -bottom-40 -right-28 pointer-events-none'>
-            <img src='/turtle_outlined.png' alt='LRNR Turtle' className='w-[550px] xl:w-[600px]' />
-        </div>
-
+            {/* === Turtle Mascot (Large screens only) === */}
+            {/* <div className='hidden lg:block absolute -bottom-40 -right-28 pointer-events-none'>
+                <img src='/turtle_outlined.png' alt='LRNR Turtle' className='w-[550px] xl:w-[600px]' />
+            </div> */}
         </footer>
     );
 }
